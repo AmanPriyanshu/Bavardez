03c3b4355a7d2d05448fc19f7822a8092c0d0009
Aman Priyanshu <aman.priyanshu@learner.manipal.edu>
2021-04-12 23:52:05 +0530

    intents init

--- intent_initializer.py
+++ intent_initializer.py
@@ -12,7 +12,7 @@ def read_all_responses():
 	return responses
 
 def create_new_intent():
-	choice = int(input("Enter \n\t0. If you wish to initialize all the intents.\n\t1. If you wish to add another intent.\n\t2. If you wish to edit one of the intents."))
+	choice = int(input("Enter:\n\t0. If you wish to initialize all the intents.\n\t1. If you wish to add another intent.\n\t2. If you wish to edit one of the intents.\nChoice:\t"))
 	if choice == 0:
 		df = pd.DataFrame(list())
 		df.to_csv('intents.csv', index=False)
