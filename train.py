05e6a50282435d037fbfb1dd3e93cc28aa084820
Aman Priyanshu <aman.priyanshu@learner.manipal.edu>
2021-04-13 01:01:36 +0530

    trainer

--- train.py
+++ train.py
@@ -2,3 +2,10 @@ import pandas as pd
 import numpy as np
 from intent_initializer import read_all_intents, read_all_responses
 
+def read_everything():
+	df_responses = read_all_responses()
+	df_patterns = read_all_patterns()
+	
+	assert len(list(df_responses.keys())) == len(list(df_patterns.keys())), "Patterns and Responses should have the same Tags"
+
+	intents = list(df_responses.keys())
