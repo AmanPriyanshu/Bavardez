132321438100031e16e3f8b2d52cef280d6a28fe
Aman Priyanshu <aman.priyanshu@learner.manipal.edu>
2021-04-13 01:04:58 +0530

    trainer

--- train.py
+++ train.py
@@ -1,11 +1,21 @@
 import pandas as pd
 import numpy as np
 from intent_initializer import read_all_intents, read_all_responses
+from preprocessing import stem, tokenize, bag_of_words
 
 def read_everything():
 	df_responses = read_all_responses()
-	df_patterns = read_all_patterns()
+	df_patterns = read_all_intents()
 	
 	assert len(list(df_responses.keys())) == len(list(df_patterns.keys())), "Patterns and Responses should have the same Tags"
 
 	intents = list(df_responses.keys())
+
+	print(df_patterns.values())
+
+	exit()
+
+	all_words = np.unique(np.array([]))
+
+if __name__ == '__main__':
+	read_everything()
