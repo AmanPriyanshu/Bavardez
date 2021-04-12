85f6a8878a7758386821b379f2168ac027a4351f
Aman Priyanshu <aman.priyanshu@learner.manipal.edu>
2021-04-13 01:29:35 +0530

    trainer

--- train.py
+++ train.py
@@ -2,6 +2,7 @@ import pandas as pd
 import numpy as np
 from intent_initializer import read_all_intents, read_all_responses
 from preprocessing import stem, tokenize, bag_of_words
+import torch
 
 def read_everything():
 	df_responses = read_all_responses()
@@ -19,11 +20,26 @@ def read_everything():
 	for sentence in all_patterns:
 		all_words.extend(tokenize(sentence))
 	all_words = sorted(set([stem(w) for w in all_words]))
-	tags = sorted(tags)
+	tags = sorted(intents)
 
 	train_x, train_y = [], []
 	for (sentence, tag) in xy:
-		bof = bag_of_words()
+		bof = bag_of_words(sentence, all_words)
+		train_x.append(bof)
+		train_y.append(tags.index(tag))
+	train_x, train_y = np.array(train_x), np.array(train_y)
+	return train_x, train_y
+
+class ChatDataset(torch.utils.Dataset):
+	def __init__(self):
+		self.train_x, self.train_y = read_everything()
+		self.n_samples = len(self.train_x)
+
+	def __getitem__(self, index):
+		return self.train_x[index], self.y_train[index]
+
+	def __len__(self):
+		return self.n_samples
 
 if __name__ == '__main__':
 	read_everything()
