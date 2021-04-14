0c773149bd0b12f3fa68d5fe8c49866fc181782a
Aman Priyanshu <aman.priyanshu@learner.manipal.edu>
2021-04-14 19:12:41 +0530

    glove trained

--- GloVe_trainer.py
+++ GloVe_trainer.py
@@ -4,6 +4,7 @@ import pandas as pd
 import numpy as np
 from intent_initializer import read_all_intents, read_all_responses
 from glove_model import get_model
+from tqdm import trange
 
 PATH = './config/'
 
@@ -20,6 +21,7 @@ def read_everything():
 	gl = GloVeLoader()
 	x_train = gl.pull_glove_embed(xy.T[1])
 	y_train = np.array([tags.index(tag) for tag in xy.T[0]])
+	y_train = y_train.astype(np.int64)
 	return x_train, y_train, tags
 
 class ChatDataset(torch.utils.data.Dataset):
@@ -37,24 +39,19 @@ def train_instance(hidden_size=8, lr=0.001, num_epochs=500):
 	dataset = ChatDataset()
 	train_loader = torch.utils.data.DataLoader(dataset=dataset, batch_size=8, shuffle=True)
 	model = get_model(50, hidden_size, len(dataset.tags))
-	print(model)
-	exit()
 	device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 	model = model.to(device)
 
 	criterion = torch.nn.CrossEntropyLoss()
 	optimizer = torch.optim.Adam(model.parameters(), lr=lr)
 
-	bar = trange(num_epochs)
+	bar = trange(num_epochs, disable=False)
 	for epoch in bar:
 		running_loss, running_accuracy = 0, 0
 		for (embeds, labels) in train_loader:
 			embeds = embeds.to(device)
 			labels = labels.to(device)
-
 			outputs = model(embeds)
-			print(outputs.shape)
-			exit()
 			loss = criterion(outputs, labels)
 
 			predictions = torch.argmax(outputs, 1)
@@ -76,7 +73,6 @@ def train_instance(hidden_size=8, lr=0.001, num_epochs=500):
 	"input_size": 50,
 	"output_size": len(dataset.tags),
 	"hidden_size": hidden_size,
-	"all_words": dataset.all_words,
 	"tags": dataset.tags
 	}
 
@@ -86,4 +82,4 @@ def train_instance(hidden_size=8, lr=0.001, num_epochs=500):
 
 
 if __name__ == '__main__':
-	train_instance(num_epochs=10)
+	train_instance()
---
+++ glove_bot.py
@@ -0,0 +1,53 @@
+import random
+import torch
+import pandas as pd
+import numpy as np
+from glove_model import get_model
+from intent_initializer import read_all_intents, read_all_responses
+from GloVe_helper import GloVeLoader
+
+PATH = './config/'
+BOT_NAME = 'Bavardez'
+
+def load_bot():
+	model_details = torch.load(PATH+'model_details_GloVe.pt')
+	model = get_model(model_details['input_size'], model_details['hidden_size'], model_details['output_size'])
+	model.load_state_dict(model_details['model_state'])
+	model.eval()
+
+	tags = model_details['tags']
+
+	return model, tags
+
+def main():
+	model, tags = load_bot()
+	df_responses = read_all_responses()
+	activation = torch.nn.Softmax(1)
+	gl = GloVeLoader()
+
+	print("Let's chat! Type \"quit\" to exit.")
+	while True:
+		sentence = input("You:\t")
+		if sentence == "quit":
+			break
+		embed = gl.pull_glove_embed([sentence])
+		print(embed.shape)
+		exit()
+		
+
+		output = model(bof)
+		probs = activation(output).flatten()
+		predicted_label = torch.argmax(probs)
+		tag = tags[predicted_label.item()]
+		if probs[predicted_label]>0.5:
+			if tag in list(df_responses.keys()):
+				answer = random.choice(df_responses[tag])
+			else:
+				answer = "Sorry there's an error in OUR SYSTEM! Please re-phrase"
+		else:
+			answer = "I do not understand you."
+		print(BOT_NAME+":\t"+answer)
+	print("Thankyou for using "+BOT_NAME)		
+
+if __name__ == '__main__':
+	main()
